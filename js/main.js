a4cd4a86c57c6667574a3a07ccde881b39ed8da7
ANSH TYAGI <anshtuffy47@gmail.cm>
2025-07-23 21:41:09 +0530

    till featured

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
+    // Close all other collapsibles before opening the new one
+    collapsibles.forEach((innerItem) => {
+      if (innerItem !== this) {
+        innerItem.classList.remove("collapsible--expanded");
+      }
+    });
+    // Toggle the clicked collapsible
     this.classList.toggle("collapsible--expanded");
   })
 );
